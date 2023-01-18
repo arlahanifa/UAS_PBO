c7b137cfd300671d9d94cc0fca479eea1fd6186e
arlahanifa <arlahanifa02@gmail.com>
2023-01-18 15:17:28 +0800

    first commit

---
+++ FDA_Arla_E020320065/app/src/main/java/id/ac/poliban/mi/vc/e020320065/fda_e020320065/Interface/ChangeNumberItemsListener.java
@@ -0,0 +1,5 @@
+package id.ac.poliban.mi.vc.e020320065.fda_e020320065.Interface;
+
+public interface ChangeNumberItemsListener {
+    void changed();
+}
---
+++ Project77_Arla_E020320065/app/src/main/java/id/ac/poliban/mi/vc/e020320065/project77_e020320065/MainActivity.java
@@ -0,0 +1,14 @@
+package id.ac.poliban.mi.vc.e020320065.project77_e020320065;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
